571cec94467a85d79964bd357ad92d2c3ff9d5cb
Sye-5pace <owusilasyp00@gmail.com>
2024-06-19 17:57:57 +0100

    feat: update ui with responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,12 @@ export default {
     fontFamily: {
       'manrope': ['Manrope', 'sans-serif']
     },
-    screen:{
-      screens: {
-        'mobile': {'min':'200px','max':'475px'},
-        'tablet': {'min':'427px','max':'782px'},
-        'laptop': {'min':'782.1px'}
-      }
+    screens: {
+      'mobile': {'min':'200px','max':'475px'},
+      'tablet': {'min':'427px','max':'782px'},
+      'laptop': {'min':'782.1px'}
     },
+
     extend: {
 
     },
