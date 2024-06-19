2664f21d04f8734719fbe0cc25d32bd2ac1911e7
Sye-5pace <owusilasyp00@gmail.com>
2024-06-19 17:19:17 +0100

    chore: update codebase

--- main.js
+++ main.js
@@ -1,32 +1,19 @@
-let advice = '';
+
 const adviceContent = document.getElementById('advice-content')
 const adviceId = document.getElementById('advice-id')
 
-
-const adviceDataFetch = async() => {
-    const adviceEntry = 'https://api.adviceslip.com/advice'
-   
+const adviceDataFetch = async () => {
     try {
-        const response = await fetch(adviceEntry);
+        const response =  await fetch('https://api.adviceslip.com/advice')
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
-        const data = await response.json();
-        advice = data;
-        console.log(advice.slip.advice)
-        console.log(advice.slip.id)
-        adviceCreator(advice)
-        return advice
+        const data = await response.json();    
+        adviceId.textContent = `#${data.slip.id}`;
+        adviceContent.textContent= `"${data.slip.advice}"`;
     } catch (error) {
         console.error('Error fetching advice:', error);
     }
 }
-
-const adviceCreator = (advice) => {
-    adviceId.textContent = `ADVICE #${advice?.slip.id}`;
-    adviceContent.textContent= `"${advice?.slip.advice}"`;
-}
-
-adviceCreator()
 
 adviceDataFetch()
