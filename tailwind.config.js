e226c5b81987648d46849656a73c9dfa4f51aed3
Sye-5pace <owusilasyp00@gmail.com>
2024-06-19 23:59:48 +0100

    style: update ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,9 @@ export default {
     },
 
     extend: {
-
+      boxShadow: {
+        'green': '0 10px 30px 30px rgba(83, 255, 170, 0.5), 0 2px 4px -1px rgba(83, 255, 170, 0.5)',
+      },
     },
   },
   plugins: [],
