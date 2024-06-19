f028c42a375f7df705d1711da5ae38a7d28cab07
Sye-5pace <owusilasyp00@gmail.com>
2024-06-19 13:07:26 +0100

    setup: setting project up, attach assets and devdependencies

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html'],
+  theme: {
+    fontFamily: {
+      'manrope': ['Manrope', 'sans-serif']
+    },
+    screen:{
+      screens: {
+        'mobile': {'min':'200px','max':'427px'},
+        'tablet': {'min':'427px','max':'782px'},
+        'laptop': {'min':'782.1px'}
+      }
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
